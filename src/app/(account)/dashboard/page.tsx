f23cfc4c657e39dbc79435edf1f2000ef3245e35
Denis K <wolf-den@tut.by>
2024-02-04 12:49:59 +0300

    feat: get data ssr ssg isr

--- src/app/(account)/dashboard/page.tsx
+++ src/app/(account)/dashboard/page.tsx
@@ -1,10 +1,18 @@
 import { cookies } from "next/headers"
 
+const getData = async () => {
+  return fetch('https://jsonplaceholder.typicode.com/todos/1', {
+    // cache: 'no-store', // SSR
+    // cache: 'force-cache', // SSG
+    next: { revalidate: 10 }, // ISR
+  })
+    .then(response => response.json())
+}
+
 export default async function DashboardPage() {
   console.log('cookies', cookies().getAll())
 
-  const data = await fetch('https://jsonplaceholder.typicode.com/todos/1')
-    .then(response => response.json())
+  const data = await getData() 
 
   return <div>{data.title}</div>
 }
