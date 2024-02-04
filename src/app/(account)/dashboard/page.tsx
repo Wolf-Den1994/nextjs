abceaaa4ab1815d7a7e40feeb94ed98e22f799b6
Denis K <wolf-den@tut.by>
2024-02-04 12:43:45 +0300

    feat: get data and cookies

--- src/app/(account)/dashboard/page.tsx
+++ src/app/(account)/dashboard/page.tsx
@@ -1,3 +1,10 @@
-export default function DashboardPage() {
-  return <div>Dashboard</div>
+import { cookies } from "next/headers"
+
+export default async function DashboardPage() {
+  console.log('cookies', cookies().getAll())
+
+  const data = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(response => response.json())
+
+  return <div>{data.title}</div>
 }
--- src/app/(account)/dashboard/profile/page.tsx
+++ src/app/(account)/dashboard/profile/page.tsx
@@ -1,3 +1,9 @@
+'use client'
+
+import { useState } from "react"
+
 export default function ProfilePage() {
+  const [isAdmin, setIsAdmin] = useState(false)
+
   return <div>Profile</div>
 }
